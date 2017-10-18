884a254028f8dc00137842b0a24b644903c99ba4
Rbatts <32482377+Rbatts@users.noreply.github.com>
2017-10-18 11:36:06 +0100

    made new database

---
+++ LIBRARY BOOKISH.sql
@@ -0,0 +1,9 @@
+
+insert into Users (FirstName, LastName, UserName, Password) values ('Rich', 'Batten', 'RichizaLegend', 'MassiveLegend')
+select * From Users
+
+insert into Books (BookTitle, Author) values ('Coding for dummies', 'a smug american prick')
+select * From Books
+
+insert into Copy (BookTitle, NumberOfCopiesAvailable, DueDate) values ('Coding for dummies', '1', '2017-10-18')
+select * From Copy
